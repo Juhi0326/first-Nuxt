17122bba11c69ce17d2126b0e61eb9739918f1cd
István Juhász <istvan.juhasz@telcotrend.hu>
2020-12-02 15:05:44 +0100

    use a logic for middleware

--- middleware/test.js
+++ middleware/test.js
@@ -1,3 +1,14 @@
-export default function () {
-  console.log('i am a middleware')
+/* eslint-disable no-console */
+export default function (context) {
+  context.$axios
+    .$post('https://reqres.in/api/login',
+      {
+        email: 'eve.holt@reqres.in',
+        password: 'cityslicka'
+      }
+    ).then((result) => {
+      console.log(result)
+    }).catch((err) => {
+      console.log(err)
+    })
 }
